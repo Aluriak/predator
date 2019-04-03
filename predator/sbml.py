2e231abbd4f91db20d711b3fe53eb4dfde4bd745
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2019-04-03 17:38:12 +0200

    refactor sbml extraction + handle topological seeds

--- predator/sbml.py
+++ predator/sbml.py
@@ -7,16 +7,22 @@ import xml.etree.ElementTree as etree
 from xml.etree.ElementTree import XML, fromstring, tostring
 
 
+def get_sbml_tag(element) -> str:
+    "Return tag associated with given SBML element"
+    if element.tag[0] == "{":
+        _, tag = element.tag[1:].split("}")  # uri is not used
+    else:
+        tag = element.tag
+    return tag
+
+
 def get_model(sbml):
     """
     return the model of a SBML
     """
     model_element = None
     for e in sbml:
-        if e.tag[0] == "{":
-            uri, tag = e.tag[1:].split("}")
-        else:
-            tag = e.tag
+        tag = get_sbml_tag(e)
         if tag == "model":
             model_element = e
             break
@@ -29,95 +35,84 @@ def get_listOfSpecies(model):
     """
     listOfSpecies = None
     for e in model:
-        if e.tag[0] == "{":
-            uri, tag = e.tag[1:].split("}")
-        else:
-            tag = e.tag
+        tag = get_sbml_tag(e)
         if tag == "listOfSpecies":
             listOfSpecies = e
             break
     return listOfSpecies
 
 
-def get_listOfReactions(model):
-    """
-    return list of reactions of a SBML model
-    """
-    listOfReactions = None
+def get_listOfReactions(model) -> list:
+    """return list of reactions of a SBML model"""
+    listOfReactions = []
     for e in model:
-        if e.tag[0] == "{":
-            uri, tag = e.tag[1:].split("}")
-        else:
-            tag = e.tag
+        tag = get_sbml_tag(e)
         if tag == "listOfReactions":
             listOfReactions = e
             break
     return listOfReactions
 
 
-def get_listOfReactants(reaction):
-    """
-    return list of reactants of a reaction
-    """
-    listOfReactants = None
+def get_listOfReactants(reaction) -> list:
+    """return list of reactants of a reaction"""
+    listOfReactants = []
     for e in reaction:
-        if e.tag[0] == "{":
-            uri, tag = e.tag[1:].split("}")
-        else:
-            tag = e.tag
+        tag = get_sbml_tag(e)
         if tag == "listOfReactants":
             listOfReactants = e
             break
     return listOfReactants
 
 
-def get_listOfProducts(reaction):
-    """
-    return list of products of a reaction
-    """
-    listOfProducts = None
+def get_listOfProducts(reaction) -> list:
+    """return list of products of a reaction"""
+    listOfProducts = []
     for e in reaction:
-        if e.tag[0] == "{":
-            uri, tag = e.tag[1:].split("}")
-        else:
-            tag = e.tag
+        tag = get_sbml_tag(e)
         if tag == "listOfProducts":
             listOfProducts = e
             break
     return listOfProducts
 
 
-def readSBMLnetwork(filename, name):
-    """Yield ASP atoms describing the network found in given SBML network"""
+def readSBMLnetwork(filename,
+                    use_topological_injections:bool=True,
+                    use_semantic_injection:bool=False):
+    """Yield ASP atoms describing the network found in given SBML network
+
+    filename -- sbml/xml file name.
+    use_topological_injections -- consider as seed any species produced by reactant-free reactions.
+    use_semantic_injection -- consider as seed any species marked as such in the SBML data.
+
+    """
     tree = etree.parse(filename)
     sbml = tree.getroot()
     model = get_model(sbml)
 
     reactions = get_listOfReactions(model)
     for e in reactions:
-        if e.tag[0] == "{":
-            uri, tag = e.tag[1:].split("}")
-        else:
-            tag = e.tag
+        tag = get_sbml_tag(e)
         if tag == "reaction":
             reactionId = e.attrib.get("id")
             yield f'dreaction("{reactionId}").'
-            if e.attrib.get("reversible") == "true":
+            reversible = e.attrib.get("reversible") == 'true'
+            if reversible:
                 yield f'reversible("{reactionId}").'
 
             reactants = get_listOfReactants(e)
-            if reactants == None:
-                print("\n Warning:", reactionId, "reactants=None")
-            else:
-                for r in reactants:
-                    yield f"reactant(\"{r.attrib.get('species')}\",\"{reactionId}\")."
-
             products = get_listOfProducts(e)
-            if products == None:
-                print("\n Warning:", reactionId, "products=None")
-            else:
-                for p in products:
-                    yield f"product(\{p.attrib.get('species')}\",\"{reactionId}\")."
+
+            for reactant in reactants:
+                name = reactant.attrib.get('species')
+                yield f'reactant("{name}","{reactionId}").'
+                if use_topological_injections and reversible and not products:  # this reaction is a generator of seed
+                    yield f'seed("{name}").'
+
+            for product in products:
+                name = product.attrib.get('species')
+                yield f'product("{name}","{reactionId}").'
+                if use_topological_injections and not reactants:  # this reaction is a generator of seed
+                    yield f'seed("{name}").'
 
 
 def readSBMLnetwork_irrev(filename, name):
