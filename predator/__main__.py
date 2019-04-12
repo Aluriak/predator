5d75295d810f7bcb982cd0d2a5f1f37484be5083
cfrioux <clemence.frioux@gmail.com>
2019-04-12 17:13:33 +0200

    fix indentation error

--- predator/__main__.py
+++ predator/__main__.py
@@ -59,7 +59,7 @@ def get_all_ids(inline, fname) -> frozenset:
     if fname:
         with open(fname) as fd:
             elements = elements | frozenset(line for line in map(str.strip, fd) if line)
-     return elements
+    return elements
 
 
 if __name__ == '__main__':
