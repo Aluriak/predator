ba38ff1ee4cb2b9f02864200f0277d1a51fb13d9
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2019-07-03 15:02:57 +0200

    example showing limits of internal SCC search
    @cfrioux
    Because an SCC is treated as a whole object, only perfectly optimized
    solutions are provided. As a consequence, even with pareto search,
    some solutions are discarded for quite obscur reasons.
    
    Without pareto, we get only `d` as solution, because most of the other
    solutions are in the same SCC, and therefore discarded as not optimal
    enough.
    
    See comments for more explanations and some solutions ideas.

--- test/test_pareto.py
+++ test/test_pareto.py
@@ -18,3 +18,30 @@ def test_simple_reaction_chain_with_double_root():
     # utils.render_network(graph, 'todel.png')  # uncomment to help debugging
     seeds_sets = {frozenset('c'), frozenset('ab')}
     assert seeds_sets == set(search_seeds(graph, targets='t', forbidden_seeds='t', explore_pareto=True))
+
+
+def test_one_scc():
+    """This show the local limits: whenever solutions are in the same SCC,
+    only the very best are yielded. As a consequence, both pareto and without
+    pareto loose some interesting (though non-optimal) solutions"""
+    graph = quoted_data("""reactant(a,1). product(b,1). reaction(1).
+                           reactant((b;c),2). product(d,2). reaction(2).
+                           reactant(d,3). product(a,3). reaction(3).
+                           reactant((d;e),4). product(f,4). reaction(4).
+                           reactant(f,5). product(c,5). reaction(5).""")
+    # utils.render_network(graph, 'todel.png')  # uncomment to help debugging
+    expected_seeds_sets = {frozenset('d'), frozenset('de')}  # 'af' would have been welcome, but does not cover as much as 'de'
+    assert expected_seeds_sets == set(search_seeds(graph, targets='d', explore_pareto=True, verbose=True, avoid_targets_as_seeds=True))
+    assert expected_seeds_sets == set(search_seeds(graph, targets='d', explore_pareto=True, verbose=True, avoid_targets_as_seeds=False))  # invariant
+    expected_seeds_sets = {frozenset('d')}  # this is… optimized
+    assert expected_seeds_sets == set(search_seeds(graph, targets='d', explore_pareto=False, verbose=True))
+
+    # to get a real sens of diversity, we need to forbids the obvious solutions
+    #  (here, just 'd', but it could be much more subtle).
+    expected_seeds_sets = {frozenset('ac'), frozenset('af'), frozenset('bc'), frozenset('bf')}
+    assert expected_seeds_sets == set(search_seeds(graph, targets='d', forbidden_seeds='d', explore_pareto=False, verbose=True))
+    expected_seeds_sets = {frozenset('af'), frozenset('bf'), frozenset('ace'), frozenset('aef'), frozenset('bce'), frozenset('bef')}
+    assert expected_seeds_sets == set(search_seeds(graph, targets='d', forbidden_seeds='d', explore_pareto=True, verbose=True))
+    # hence, this test ask a question: do another metric for the greedy search
+    #  in an SCC is needed ? Probably.
+    # Something like a constraint providing *a maximal number/ratio of seed*.
